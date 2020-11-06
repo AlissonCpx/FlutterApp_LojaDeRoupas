fd014fca5dc76db12a141a67d7e6405d2aeca611
AlissonCpx <alissondeandrade1@gmail.com>
2020-11-06 18:45:27 -0300

    Enviando as alterações para mim continuar no notebook

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_loja_virtual/tabs/home_tab.dart';
+import 'package:flutter_loja_virtual/tabs/products_tab.dart';
 import 'package:flutter_loja_virtual/widgets/custom_drawer.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -15,7 +16,15 @@ class HomeScreen extends StatelessWidget {
        Scaffold(
          body:  HomeTab(),
          drawer: CustomDrawer(_pageController),
-       )
+       ),
+        Scaffold(
+          appBar: AppBar(
+            title: Text("Produtos"),
+            centerTitle: true,
+          ),
+          drawer: CustomDrawer(_pageController),
+          body: ProductsTab(),
+        )
       ],
     );
   }
